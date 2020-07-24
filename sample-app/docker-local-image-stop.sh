2d870237975bc0532f0a02007321926eb9c27452
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-07-24 16:42:28 +0530

    Completed scripts

---
+++ sample-app/docker-local-image-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker stop reactdemo || true
+docker rm reactdemo || true
+
---
+++ sample-app/environmentConfig.js
@@ -0,0 +1,4 @@
+var config = {
+  siteName: "View Linked In",
+  siteAddress: "https://www.linkedin.com/"
+};
